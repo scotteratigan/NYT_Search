7a4d301fcd9606a3f134c65f386bda2d5f8f50ea
ScottE <scotteratigan@gmail.com>
2018-10-20 13:45:35 -0700

    Added clear btn

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ console.log("app.js is linked");
 
 $("#search-button").on('click', runSearch);
 
+$("#clear-button").on('click', clearSearchFields);
+
 function runSearch() {
 	var searchTerm = $("#search-term");
 	var numberOfRecords = $("#number-of-records");
 	var startYear = $("#start-year");
 	var endYear = $("#end-year");
+}
+
+function clearSearchFields() {
+	$("#search-term").empty();
+	$("#number-of-records").empty();
+	$("#start-year").empty();
+	$("#end-year").empty();
 }
