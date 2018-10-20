7036fbff4d33f1d38734d93490189aa1f2ab90af
ScottE <scotteratigan@gmail.com>
2018-10-20 13:43:02 -0700

    backend update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("app.js is linked");
+
+$("#search-button").on('click', runSearch);
+
+function runSearch() {
+	var searchTerm = $("#search-term");
+	var numberOfRecords = $("#number-of-records");
+	var startYear = $("#start-year");
+	var endYear = $("#end-year");
+}
